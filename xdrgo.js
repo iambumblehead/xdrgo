03965daaef9686e7c6f578e27c78ba8a788935eb
chris <chris@bumblehead.com>
2016-01-11 11:51:44 -0800

    open xhr object before setting headers. set minimal headers

--- xdrgo.js
+++ xdrgo.js
@@ -1,5 +1,5 @@
 // Filename: xdrgo.js  
-// Timestamp: 2016.01.11-11:22:38 (last modified)
+// Timestamp: 2016.01.11-11:51:10 (last modified)
 // Author(s): Bumblehead (www.bumblehead.com)  
 // Requires: xhrgo.js
 //
@@ -12,26 +12,36 @@ var xdrgo = module.exports = (function (xdrgo) {
   
   xdrgo = Object.create(xhrgo);
 
+  xdrgo.newRequest = function () {
+    var xdr = xhrgo.newRequest();
+    
+    if ("withCredentials" in xdr) {
+      xdr = xdr;
+    } else if (typeof XDomainRequest === "function") {
+      xdr = new XDomainRequest();
+    } else {
+      xdr = null;
+    }
+
+    return xdr;
+  };
+
   xdrgo.quickJSON = function (type, uri, data, token, fn, resWaitTime) {
     var xhr = xdrgo.newRequest(), 
         timeout = resWaitTime || 30000, 
         finData, timer,
         doneFn = function (err, res) { if (typeof fn === 'function') fn(err, res); };
 
-    if ("withCredentials" in xhr) {
+    if (xhr) {
       xhr.open(type, uri, true);
-    } else if (typeof XDomainRequest === "function") {
-      xhr = new XDomainRequest();
-      xhr.open(type, uri);
     } else {
-      // Otherwise, CORS is not supported by the browser.
       return fn(new Error('[!!!] CORS is not supported'));
     }
 
     if (token) {
       xhr.withCredentials = true;
     }
-
+    
     xhr.setRequestHeader("Accept", "application/json, text/javascript");
     if (type.match(/PUT|POST/) && typeof data === 'object' && data) {
       finData = JSON.stringify(data);
